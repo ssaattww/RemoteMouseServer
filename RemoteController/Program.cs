2a077ce37c94c0a2cfc861aa376e6c9a4f628890
ssaattww <taiga202500@gmail.com>
2022-09-10 10:15:42 +0900

    Add function to auto select m5stack core2 COM.

--- RemoteController/Program.cs
+++ RemoteController/Program.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.IO.Ports;
+using System.Management;
 using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 
 namespace RemoteController
 {
+    [SupportedOSPlatform("windows")]
     class PortDataReceived
     {
         public static void Main()
         {
+            var devNames = GetDeviceNames().ToList();
+            var regComNo = new System.Text.RegularExpressions.Regex("(COM[1-9][0-9]?[0-9]?)");
+            var regM5StackDevName = new System.Text.RegularExpressions.Regex("CP210x");
+            var m5stackCOM = devNames
+                .Where(devName => regM5StackDevName.IsMatch(devName))
+                .Select(devName => regComNo.Match(devName).Value).FirstOrDefault();
+            Console.WriteLine(m5stackCOM);
             SerialPort mySerialPort = new SerialPort("COM11");
 
             mySerialPort.BaudRate = 115200;
@@ -53,6 +63,50 @@ namespace RemoteController
             string indata = sp.ReadExisting();
             Console.WriteLine("Data Received:");
             Console.Write(indata);
+        }
+        public static string[] GetDeviceNames()
+        {
+            var deviceNameList = new System.Collections.ArrayList();
+            var check = new System.Text.RegularExpressions.Regex("(COM[1-9][0-9]?[0-9]?)");
+
+            ManagementClass mcPnPEntity = new ManagementClass("Win32_PnPEntity");
+            ManagementObjectCollection manageObjCol = mcPnPEntity.GetInstances();
+
+            //全てのPnPデバイスを探索しシリアル通信が行われるデバイスを随時追加する
+            foreach (ManagementObject manageObj in manageObjCol)
+            {
+                //Nameプロパティを取得
+                var namePropertyValue = manageObj.GetPropertyValue("Name");
+                if (namePropertyValue == null)
+                {
+                    continue;
+                }
+                else
+                {
+                    //Nameプロパティ文字列の一部が"(COM1)～(COM999)"と一致するときリストに追加"
+                    string name = namePropertyValue.ToString();
+                    if (check.IsMatch(name))
+                    {
+                        deviceNameList.Add(name);
+                    }
+                }
+            }
+
+            //戻り値作成
+            if (deviceNameList.Count > 0)
+            {
+                string[] deviceNames = new string[deviceNameList.Count];
+                int index = 0;
+                foreach (var name in deviceNameList)
+                {
+                    deviceNames[index++] = name.ToString();
+                }
+                return deviceNames;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 
